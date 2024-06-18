450e7c4fada7accca48f7e06d2bf1c9c29adc42c
Paul Wentzel <paul.wentzel@gmail.com>
2024-06-17 20:26:19 -0400

    all tests passing

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -11,6 +11,9 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   constructor(numBuckets = 8) {
     // Initialize your buckets here
     // Your code here
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0;
   }
 
   hash(key) {
@@ -31,21 +34,96 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   insert(key, value) {
     // Your code here
+    if (((this.count + 1) / this.capacity) > 0.7) {
+      this.resize;
+    }
+
+    const idx = this.hashMod(key);
+
+    if (!this.data[idx]) {
+      this.data[idx] = new KeyValuePair(key, value);
+      this.count++;
+    } else {
+      let curr = this.data[idx];
+      let found = false;
+
+      while (curr) {
+        if (curr.key === key) {
+          curr.value = value;
+          found = true;
+          break;
+        }
+        curr = curr.next;
+      }
+
+      if (!found) {
+        const pair = new KeyValuePair(key, value)
+        pair.next = this.data[idx];
+        this.data[idx] = pair;
+        this.count++;
+      }
+    }
   }
 
 
   read(key) {
     // Your code here
+    const idx = this.hashMod(key);
+    let value;
+    let curr = this.data[idx];
+
+    while (curr) {
+      if (curr.key === key) {
+        value = curr.value;
+        break;
+      }
+      curr = curr.next;
+    }
+    return value;
   }
 
 
   resize() {
     // Your code here
+    this.capacity = this.capacity * 2;
+    this.count = 0;
+
+    const oldData = this.data;
+    this.data = new Array(this.capacity).fill(null);
+
+    oldData.forEach(pair => {
+      while (pair) {
+        this.insert(pair.key, pair.value)
+        pair = pair.next;
+      }
+    })
+
   }
 
 
   delete(key) {
     // Your code here
+    const idx = this.hashMod(key);
+    let prev = null;
+    let curr = this.data[idx];
+    let found = false;
+    while (curr) {
+      if(curr.key === key) {
+        found = true;
+        if(prev) {
+          prev.next = curr.next;
+        } else {
+          this.data[idx] = curr.next;
+        }
+        this.count--;
+      }
+      prev = curr;
+      curr = curr.next;
+    }
+
+    if (!found){
+      return "Key not found"
+    }
   }
 }
 
--- phases/02-practice-problems.js
+++ phases/02-practice-problems.js
@@ -1,26 +1,68 @@
 function anagrams(str1, str2) {
   // Your code here
+  let letters = {};
+  for (const c of str1) {
+    letters[c] = letters[c] ? letters[c] + 1 : 1;
+  }
+
+  for (const c of str2) {
+    if (letters[c] === undefined) return false;
+    letters[c] -= 1;
+    if (letters[c] < 0) return false;
+  }
+  return true;
 }
 
 
 function commonElements(arr1, arr2) {
   // Your code here
+  const set1 = new Set(arr1);
+  let rv = [];
+  for (const el of arr2) {
+    if (set1.has(el))
+      rv.push(el);
+  }
+  return rv;
 }
 
 
 function duplicate(arr) {
   // Your code here
+  const set = new Set();
+  for (const el of arr){
+    if (set.has(el)) return el;
+    set.add(el);
+  }
+
 }
 
 
 function twoSum(nums, target) {
   // Your code here
+  const set = new Set(nums);
+  for (const el of nums) {
+    if (target / 2 === el) continue;
+    if (set.has(target - el)) return true;
+  }
+  return false;
+
 }
 
 
 function wordPattern(pattern, strings) {
   // Your code here
+  const obj = {};
+  const set = new Set();
+  for (let i = 0; i < pattern.length; i++) {
+    if (set.has(strings[i]) && obj[pattern[i]] !== strings[i]) return false;
+    if (obj[pattern[i]]) {
+      if (obj[pattern[i]] !== strings[i]) return false;
+    } else {
+      obj[pattern[i]] = strings[i];
+      set.add(strings[i]);
+    }
+  }
+  return true;
 }
-
 
 module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
